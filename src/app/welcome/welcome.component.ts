53a9c47f6dff8455405731c1cf2c3801896f58af
Mariia Yelisieieva <maria97.55ua@gmail.com>
2020-07-10 20:28:29 +0300

    logged-in welcome changes

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../authorization/services/auth.service';
+import { AuthorsService } from '../services/authors.service';
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+  name: string;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    if (this.authService.isAuthenticated()) {
+      this.name = this.authService.name;
+    }
   }
 
 }
